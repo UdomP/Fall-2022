0aea20ae97af9c8dae3c22dea3824ef8fd5b9068
UdomP <71354146+UdomP@users.noreply.github.com>
2022-10-03 19:02:58 -0400

    more

--- Machine Learning/Assignment_3/assignment_3-2.py
+++ Machine Learning/Assignment_3/assignment_3-2.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-import random
+import numpy as np
 
 vocabTraining = {}
 vocabTesting = {}
@@ -10,6 +10,8 @@ trainingFileN = []
 trainingFileS = []
 testingFileN = []
 testingFileS = []
+trainingOutput = []
+testingOutput = []
 
 def readFileTraining(path):
     infile=open(path,'r')
@@ -57,7 +59,7 @@ def readEachFile():
         trainingFileN.append("email/normal/" + i)
     for j in spamEmailList:
         readFileTraining("email/spam/" + j)
-        trainingFileS.append("email/normal/" + i)
+        trainingFileS.append("email/spam/" + i)
 
 def readFile(path):
     sList = []
@@ -70,7 +72,6 @@ def wordOfVector(dic, pathList):
     sentenceWoV = []
     for p in pathList:
         pList = readFile(p)
-        print(pList)
         sen = []
         for k in dic:
             if k in pList:
@@ -80,23 +81,88 @@ def wordOfVector(dic, pathList):
         sentenceWoV.append(sen)
     return sentenceWoV
 
+def h(x, t, index):
+    thetaX = np.dot(np.transpose(t),x[index])
+    return 1/(1 + np.exp(-thetaX))
+
+def gradientAsscent(x, y, t, alpha):
+    newTheta = t
+    for j in range(len(t)):
+        s = 0
+        for i in range(len(y)):
+            s += (y[i] - h(x, t, i)) * x[i][j]
+        newTheta[j] = newTheta[j] + alpha * (s/len(y))
+        # newTheta[j] = newTheta[j] + alpha * (s)
+    return newTheta
+
+def train(iteration, x, y, alpha, t):
+    th = t
+    for i in range(iteration):
+        th = gradientAsscent(x, y, th, alpha)
+    return th
+
+def predict(x, y, t):
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+    for i in range(len(y)):
+        actual = y[i]
+        predict = np.round(h(x, t, i))
+        if(actual == 1 and predict == 1):
+            TP += 1
+        elif(actual == 0 and predict == 0):
+            TN += 1
+        elif(actual == 0 and predict == 1):
+            FP += 1
+        elif(actual == 1 and predict == 0):
+            FN += 1
+    return TP,TN,FP,FN
 
 readEachFile()
 dfTraining = pd.DataFrame(list(vocabTraining.items()), columns = ['Key','Value'])
 dfTesting = pd.DataFrame(list(vocabTesting.items()), columns = ['Key','Value'])
 
-print(trainingFileN)
-print(trainingFileS)
-print(testingFileN)
-print(testingFileS)
+classifiedSentences = (wordOfVector(vocabTraining, (trainingFileN + trainingFileS)))
+newClassifiedSentences = []
+for i in classifiedSentences:
+    x = i
+    x.insert(0, 1)
+    newClassifiedSentences.append(x)
 
-print(vocabTraining)
-print(trainingFileN[:2])
-print(wordOfVector(vocabTraining, trainingFileN[:2]))
-# sen = []
-# for a in vocabTraining:
-#     if a in sentenceTraining[1]:
-#         sen.append(1)
-#     else:
-#         sen.append(0)
-# print(sen)
+sentencesNpArray = np.asarray(newClassifiedSentences)
+
+classifiedSentencesTR = (wordOfVector(vocabTesting, (testingFileN + testingFileS)))
+newClassifiedSentencesTR = []
+for i in classifiedSentencesTR:
+    x = i
+    x.insert(0, 1)
+    newClassifiedSentencesTR.append(x)
+
+sentencesNpArray = np.asarray(newClassifiedSentences)
+sentencesNpArrayTR = np.asarray(newClassifiedSentencesTR)
+
+theta = np.asarray([.5,.5,.5,.5])
+alpha = .005
+iteration = 400
+
+print('Maximum Likelihood Estimation with normal Data')
+print("Alpha = " + str(alpha))
+print("Iteration = " + str(iteration))
+theta = train(iteration, trainingData, trainingOutput, alpha, theta)
+print('Theta = ', end = '')
+print(theta)
+
+TP,TN,FP,FN = predict(testingData, testingOutput, theta)
+print("True Positive = " + str(TP))
+print("True Negative = " + str(TN))
+print("False Positive = " + str(FP))
+print("False Negative = " + str(FN))
+print("Total = " + str(TP+TN+FP+FN))
+
+precision = TP/(TP + FP)
+recall = TP/(TP + FN)
+print("precision = " + str(precision))
+print("recall = " + str(recall))
+f1Score = 2 * (precision * recall)/(precision + recall)
+print("F1-Score = " + str(f1Score))
