f0e23219225910414ebedb6005b9d6629d8e9d08
UdomP <sereyudomp@gmail.com>
2022-11-08 17:39:06 -0500

    more

--- Machine Learning/Assignment_4/Assignment_4-1.py
+++ Machine Learning/Assignment_4/Assignment_4-1.py
@@ -25,8 +25,8 @@ for cd in crabData:
     crabDicTraining[cd] = crabData[cd][:int(crabDataSize*.9)]
     crabDicTesting[cd] = crabData[cd][int(crabDataSize*.9):]
 # ['Sex', 'Length', 'Diameter', 'Height', 'Weight', 'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Age']
-# featureList = ['Length', 'Diameter', 'Height', 'Weight', 'Shucked Weight', 'Viscera Weight', 'Shell Weight']
-featureList = ['Length', 'Height']
+featureList = ['Length', 'Diameter', 'Height', 'Weight', 'Shucked Weight', 'Viscera Weight', 'Shell Weight']
+# featureList = ['Length', 'Height']
 
 trainingLen = int(crabDataSize*.9)
 testingLen = int(crabDataSize*.1)
@@ -47,6 +47,7 @@ weightMatrix2 = np.ones((nnFeatureLen, nnFeatureLen)) * .5
 weightMatrix3 = np.ones((1, nnFeatureLen)) * .5
 
 # weightMatrix = np.random.uniform(0,1,(nnFeatureLen, nnFeatureLen))
+# print(weightMatrix)
 zMatrix = np.zeros((nnFeatureLen, 1))
 
 JList = []
@@ -61,12 +62,38 @@ def getZ(wMatrix, xMatrix):
 def sigmoid(z, x):
     return (1/(1 + (np.exp(-(np.transpose(z) @ x)))))
 
+def sigmoid(z):
+    return (1/(1 + (np.exp(-(z)))))
+
 def fx(zM):
     temp = np.exp(-zM)
     return 1/(1+temp)
 
 def yHat(w, a):
     return w @ a
+# , w2, w3, x, y, yhat, z1
+def layer1Update(w1, w2, w3, z1, z2, x, y, yhat):
+    tempW1 = w1.copy()
+    for rowIndex, row in enumerate(w1):
+        jz = yhat - y
+        a1z1 = sigmoid(z1[rowIndex])
+        z1w11 = x[rowIndex]
+        const = jz * a1z1 * z1w11
+        for colIndex, col in enumerate(row):
+            s = 0
+            for i in range(nnFeatureLen):
+                s += w3[0][i] * sigmoid(z2[i]) * (1 - sigmoid(z2[i])) * w2[rowIndex][i]
+                # s += w3[0][i]
+                # s += sigmoid(z2[i])
+                # s += (1 - sigmoid(z2[i]))
+                # s += (w2[rowIndex][i])
+            tempW1[rowIndex][colIndex] = const * s
+    return tempW1
+
+def layer2Update(w1, w2, w3, z1, z2, x, y, yhat):
+    tempW2 = w2.copy()
+    for rowIndex, row in enumerate(w2):
+        for colIndex, col in enumerate(row):
 
 # def J(y, w, x):
 #     tempY = y.copy()
@@ -83,16 +110,25 @@ def J(y, yHat):
     temp = np.abs(y - yHat)
     return  0.5 * (temp**2)
 
-for i in range(trainingLen):
-    tempTraining = training[:, i].reshape(len(training),1)
-    tempOutput = output[:, i].reshape(len(output),1)
+def neuralNetwork():
+    j = 0
+    for i in range(trainingLen):
+        tempTraining = training[:, i].reshape(len(training),1)
+        tempOutput = output[:, i].reshape(len(output),1)
+        global weightMatrix
 
-    z = getZ(weightMatrix, training[:, i])
-    a = fx(z)
-    
-    z2 = getZ(weightMatrix2, a)
-    a2 = fx(z2)
-    yh = yHat(weightMatrix3, a2)
-    j = J(tempOutput, yh)
-    print(yh)
-    print(j)
+        z = getZ(weightMatrix, tempTraining)
+        a = fx(z)
+
+        z2 = getZ(weightMatrix2, a)
+        a2 = fx(z2)
+        
+        yh = yHat(weightMatrix3, a2)
+        j = J(tempOutput, yh)
+        # print(yh)
+        # print(j)
+        tempWeight = layer1Update(weightMatrix, weightMatrix2, weightMatrix3, z, z2, tempTraining, tempOutput, yh)
+        weightMatrix = weightMatrix - (alpha * tempWeight)
+    return j
+
+print(neuralNetwork())
